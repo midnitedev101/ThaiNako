466f892a5d1a0ec97fcd7edb405bb9ea777fd93e
Alex Maglonso <midnitedev101@gmail.com>
2019-12-25 16:35:55 -0800

    First heroku commit for ThaiNako project

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/dessert'
-  get 'static_pages/drinks'
-  get 'static_pages/entrees'
-  get 'static_pages/appetizers'
-  get 'static_pages/contact'
+  # get 'static_pages/home'
+  # get 'static_pages/dessert'
+  # get 'static_pages/drinks'
+  # get 'static_pages/entrees'
+  # get 'static_pages/appetizers'
+  # get 'static_pages/contact'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root "application#hello"
+
+  get 'dessert' => 'static_pages#dessert'
+  get 'drinks' => 'static_pages#drinks'
+  get 'entrees' => 'static_pages#entrees'
+  get 'appetizers' => 'static_pages#appetizers'
+  get 'contact' => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#hello"
+  root "static_pages#home"
 end
